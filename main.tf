5fd86eb803cef77b8d5fbc196f8032690e221dca
B <aubreybrianne@bris-air.mynetworksettings.com>
2026-05-30 14:36:10 -0400

    Fix EC2 public IP association and update main.tf

--- main.tf
+++ main.tf
@@ -179,10 +179,11 @@ data "aws_ami" "amazon_linux_2023" {
 # EC2 Instance
 resource "aws_instance" "wordpress" {
   ami                    = data.aws_ami.amazon_linux_2023.id
-  instance_type          = "t2.micro"
+  instance_type          = "t3.micro"
   subnet_id              = aws_subnet.public_1.id
   vpc_security_group_ids = [aws_security_group.ec2_sg.id]
   key_name               = var.key_pair_name
+  associate_public_ip_address = true
 
   user_data = <<-EOF
               #!/bin/bash
